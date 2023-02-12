9a21fd241917b8e710b27db925e46aea8ca5af92
slows14tem <slows14@naver.com>
2023-02-12 15:09:03 +0900

    2023-02-12

--- front2/src/API/index.js
+++ front2/src/API/index.js
@@ -5,9 +5,18 @@ import axios from 'axios'
 const BASE_URL = "http://3.35.179.46:8080/data/"
 
 const axiosAPI = (url, options) => {
-  const instance = axios.create({ baseURL: url, ...options })
+  const instance = axios.create({ baseURL: url, ...options, 
+    headers: {
+      "Content-Type": `application/json;charset=UTF-8`,
+      "Accept": "application/json",
+      // "Authorization": "Bearer "+token,
+    
+      // 추가  
+      "Access-Control-Allow-Origin": `http://3.35.179.46:3000`,
+      'Access-Control-Allow-Credentials':"true",
+  } })
   // instance.defaults.withCredentials = true;
-  instance.defaults.headers = { 'Access-Control-Allow-Origin': '*'}
+  
 
   return instance
 }
