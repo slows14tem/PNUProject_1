063a23c16bb2f13f49283bebe0374648d162c25c
slows14tem <slows14@naver.com>
2023-02-12 12:16:06 +0900

    2023-02-12

--- project_1/src/main/java/edu/pnu/config/CorsConfig.java
+++ project_1/src/main/java/edu/pnu/config/CorsConfig.java
@@ -2,15 +2,17 @@ package edu.pnu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://3.35.179.46:8080")
+			.allowedOrigins("http://3.35.179.46:8080", "http://3.35.179.46:3000")
 			.allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
