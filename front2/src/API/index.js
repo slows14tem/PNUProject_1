2438d702f92a66472d003fff692cf9230f5c4478
slows14tem <slows14@naver.com>
2023-02-12 13:27:39 +0900

    2023-02-12

--- front2/src/API/index.js
+++ front2/src/API/index.js
@@ -3,13 +3,15 @@ import axios from 'axios'
 //axios 인스턴스 생성
 // const BASE_URL = "http://localhost:8080/data/"
 const BASE_URL = "http://3.35.179.46:8080/data/"
-axios.defaults.withCredentials = true;
-axios.defaults.headers.origins = "http://3.35.179.46:3000"
+// axios.defaults.withCredentials = true;
+// axios.defaults.headers.origins = "http://3.35.179.46:3000"
 
 
 const axiosAPI = (url, options) => {
   const instance = axios.create({ baseURL: url, ...options })
   return instance
 }
+instance.defaults.withCredentials = true;
+instance.defaults.headers = "http://3.35.179.46:3000";
 
 export const defaultInstance = axiosAPI(BASE_URL)
