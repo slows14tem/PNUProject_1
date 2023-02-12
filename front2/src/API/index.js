0a967e9913a42315dc53fe5b181511aa9ccf9e2d
slows14tem <slows14@naver.com>
2023-02-13 00:48:13 +0900

    2023-02-12

--- front2/src/API/index.js
+++ front2/src/API/index.js
@@ -4,7 +4,10 @@ import axios from 'axios'
 // const BASE_URL = "http://localhost:8080/data/"
 const BASE_URL = "http://3.35.179.46:8080/data/"
 const header = {
-  'Access-Control-Allow-Origin': 'http://3.35.179.46:8080'
+  'Access-Control-Allow-Origin': 'http://3.35.179.46:8080',
+  'Access-Control-Allow-Methods': 'POST, PUT, PATCH, GET, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
+  'Access-Control-Allow-Credentials': 'true'
 }
 
 const axiosAPI = (url, header, options) => {
